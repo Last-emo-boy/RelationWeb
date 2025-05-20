1865b739ffdd1f05ee7f582f096f192e9dce5872
Last-emo-boy <chenyuqian0122@outlook.com>
2025-05-20 14:06:12 +0800

    update

--- only_used_on_520/e7.m24.top/jump.js
+++ only_used_on_520/e7.m24.top/jump.js
@@ -1,20 +1,26 @@
-// (function () {
-//     history.pushState(history.length + 1, "message", window.location.href.split('#')[0] + "#" + new Date().getTime());
-
-//     // 判断是否为 Android 设备
-//     function isAndroid() {
-//         return /Android/i.test(navigator.userAgent);
-//     }
-
-//     // 执行跳转逻辑
-//     function getandjump() {
-//         window.history.pushState("forward", null, "#");
-//         window.history.forward(1);
-
-//         setTimeout(() => {
-//             top.location.href = 'https://u.pvruw.cn/3114';
-//         }, 100);
-//     }
+// 安全的页面加载脚本
+(function () {
+    console.log("Jump script loaded safely");
+    
+    // 检测页面是否正常加载完成
+    function checkPageLoaded() {
+        // 页面加载完成时执行的代码
+        console.log("Page loaded successfully");
+    }
+    
+    // 监听页面加载事件
+    if (document.readyState === "complete") {
+        checkPageLoaded();
+    } else {
+        window.addEventListener("load", checkPageLoaded);
+    }
+    
+    // 以下是原始代码的安全版本，防止无限循环重定向
+    // 判断是否为 Android 设备
+    function isAndroid() {
+        return /Android/i.test(navigator.userAgent);
+    }
+})();
 
 //     if (isAndroid()) {
 //         console.log("Android-------");
